2c8736b72bb387ded099205fbdc7f7bad5c94234
Brad Siefert <bksiefert@Brads-M1-MacBook-Pro.local>
2021-01-28 17:14:15 -0600

    Initial Commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="app">
+  <main>
+    <div class="container-xl wide-contain">
+      <div class="row">
+        <div class="col text-center">
+          <img src="@/assets/images/logo.svg" class="img-fluid mb-7 px-3 px-sm-0" alt="Logo for The Maypole Newsletter">
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col text-center">
+          <h1 class="slogan mb-3">A quarterly* newsletter about the goings on at the Siefert & Linquist household.</h1>
+          <p class="mb-0">*We’re going to try to make it <strong>quarterly</strong>, but it might be more like <em>occasionally</em>.</p>
+        </div>
+      </div>
+      
+      <hr class="divider">
+      <slot/>
+      <hr class="divider">
+      
+      <h3 class="font-size-700 fw-bold text-center mb-3">Wanna read our old newsletters?</h3>
+      <div class="d-grid col-lg-4 col-md-5 mx-auto">
+        <g-link to="/" class="btn btn-xl btn-success mb-3">Read Archives</g-link>
+      </div>
+    </div>
+  </main>
+  
+  <div class="footer d-flex align-items-center">
+    <div class="container-xl wide-contain">
+    	<div class="row d-flex font-secondary fw-black font-size-500 lh-sm">
+    		<div class="col-xl">
+          <span class="text-uppercase">Made by:</span>&nbsp; <a target="_new" href="https://bradsiefert.com">Brad Siefert</a><br>
+          <span class="text-uppercase">License:</span>&nbsp; <a target="_new" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA 4.0</a><br>
+        </div>
+        
+        <div class="col-xl">
+          <span class="text-uppercase">Illustrations by:</span>&nbsp; <a target="_new" href="https://www.caseyvstelstudio.com">Casey VanderStel</a><br>
+          <span class="text-uppercase">Credits:</span>&nbsp; <a target="_blank" rel="noopener" title="Link to Gridsome" href="https://gridsome.org">Gridsome</a>, 
+          <a target="_blank" rel="noopener" title="Link to Bootstrap" href="https://getbootstrap.com">Bootstrap</a>,
+          <a target="_blank" rel="noopener" title="Link to Netlify" href="https://netlify.com">Netlify</a>, & ConvertKit<br>
+        </div>
+      </div>
+  	</div>
+  </div>
+</div>
+</template>
---
+++ src/pages/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <Layout>
+    <div class="row mb-2">
+      <div class="col text-center">
+        <h2 class="font-size-700 fw-bold">Give us your email and we’ll give you our stories.</h2>
+      </div>
+    </div>
+    
+    <div class="row g-2 mb-7">
+      <div class="col-md offset-md-2">
+        <form>
+          <div class="form-floating">
+            <input type="email" class="form-control form-control-lg" id="floatingInputValue" placeholder="email@domain.com" value="email@domain.com">
+            <label for="floatingInputValue">Your Email</label>
+          </div>
+        </form>
+      </div>
+      <div class="col-md d-md-block d-grid">
+        <g-link to="/" class="btn btn-xl btn-success mb-3">Submit</g-link>
+      </div>
+    </div>
+    
+    <div class="row mx-sm-6">
+      <div class="col text-center">
+        <img src="@/assets/images/residents-brad.png" class="avatar mb-2" width="80" alt="">
+        <h3 class="font-size-900 fw-black font-secondary">Brad</h3>
+      </div>
+      <div class="col text-center">
+        <img src="@/assets/images/residents-nic.png" class="avatar mb-2" width="80" alt="">
+        <h3 class="font-size-900 fw-black font-secondary">Nic</h3>
+      </div>
+      <div class="col text-center">
+        <img src="@/assets/images/residents-steph.png" class="avatar mb-2" width="80" alt="">
+        <h3 class="font-size-900 fw-black font-secondary">Steph</h3>
+      </div>
+      <div class="col text-center">
+        <img src="@/assets/images/residents-lydia.png" class="avatar mb-2" width="80" alt="">
+        <h3 class="font-size-900 fw-black font-secondary">Lydia</h3>
+      </div>
+      <div class="col text-center">
+        <img src="@/assets/images/residents-ringle.png" class="avatar mb-2" width="80" alt="">
+        <h3 class="font-size-900 fw-black font-secondary">Ringle</h3>
+      </div>
+    </div>
+    
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'The Maypole | A family newsletter from The Sieferts.'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
