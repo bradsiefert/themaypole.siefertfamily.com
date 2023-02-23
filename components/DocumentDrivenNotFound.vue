eb9aa516265e12078ff369b187d8c2908cfbc592
Brad Siefert <bradsiefert@icloud.com>
2023-02-23 16:01:52 -0600

    Adding 404

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="row text-center mb-5">
+    <h1 class="text-center fw-bold">Ooops! 404 Error</h1>
+    <p class="font-size-600">
+      Sorry about that. <NuxtLink to="/">Go back home</NuxtLink>.
+    </p>
+  </div>
+</template>
